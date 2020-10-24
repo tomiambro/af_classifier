f01bb55f29c9c08ac8dd7995d08528305ac0dc76
Tomi <tomi.ambro94@gmail.com>
2020-10-24 17:10:14 -0300

    Added separate notebooks for eda and model selection

--- af_app.py
+++ af_app.py
@@ -34,6 +34,7 @@ st.title('Atrial Fibrilation Detector')
 	Although data from 12-lead ECG was provided, for this first analysis we've only used the lead 2 data and we've processed
 	the signals in order to create a dataframe consisting of features we believe will help us classify.
 """
+
 @st.cache
 def load_data(lead='lead2-HRV'):
 	df = pd.read_feather(f'datasets/phys-raw-{lead}-corrected')
@@ -41,15 +42,25 @@ def load_data(lead='lead2-HRV'):
 	df.loc[df.label != 'AF', 'label'] = 'Non-AF'
 	return df
 
+@st.cache
+def filter_df(df_raw):
+	df = df_raw.copy()
+	cols = df_raw.columns
+	cols = cols.drop('label')
+	for col in cols:
+	    df = df[df[col] < df[col].quantile(.99)]
+	df_raw = df.copy()
+	return df_raw
+
 df_raw = load_data()
 
 st.header("Dendrogram")
 corr = np.round(spearmanr(df_raw.drop('label', axis=1)).correlation, 4)
 corr_condensed = hc.distance.squareform(1 - corr)
 z = hc.linkage(corr_condensed, method='average')
-fig = plt.figure(figsize=(16,10))
+fig_den = plt.figure(figsize=(16,10))
 dendrogram = hc.dendrogram(z, labels=df_raw.drop('label',axis=1).columns, orientation='left', leaf_font_size=16)
-st.pyplot(fig)
+st.pyplot(fig_den, clear_figure=True)
 
 """
 We drop HRV_SDSD
@@ -57,25 +68,39 @@ We drop HRV_SDSD
 to_drop = ['HRV_SDSD']
 df_raw = df_raw.drop(to_drop, axis=1)
 
-st.header("Correlation")
-fig = plt.figure(figsize=(16,10))
+st.header("Correlation Matrix")
+fig_cor = plt.figure(figsize=(16,10))
 sns.heatmap(df_raw.corr())
-st.pyplot(fig)
+st.pyplot(fig_cor, clear_figure=True)
 
 st.header("Boxplots")
-df_raw = df_raw.drop(5685)
-fig = plt.figure(figsize=(20,5))
+fig_box1 = plt.figure(figsize=(20,5))
 sns.boxplot(data=df_raw)
-st.pyplot(fig)
-
+st.pyplot(fig_box1, clear_figure=True)
 
 """
-Let's remove some outliers
+### Let's remove some outliers
+(We keep everything below the 99th percentile)
 """
-slider = st.slider("", 800, 10100, 10100, 1000)
-df_raw.loc[df_raw.mean_P_Peaks > slider,'mean_P_Peaks']
-df_raw = df_raw.drop(df_raw.loc[df_raw.mean_P_Peaks > slider,'mean_P_Peaks'].index)
 
-fig = plt.figure(figsize=(20,5))
-sns.boxplot(data=df_raw['mean_P_Peaks'])
-st.pyplot(fig)
+#slider = st.sidebar.slider("", 800, 10100, 10100, 100)
+df_raw = filter_df(df_raw)
+
+fig_box2 = plt.figure(figsize=(20,5))
+sns.boxplot(data=df_raw)
+st.pyplot(fig_box2, clear_figure=True)
+
+y = df_raw['label']
+X = df_raw.drop('label', axis=1)
+X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.2, random_state=42)
+res = pd.DataFrame({'model':[], 'f1':[]})
+
+models = [LogisticRegression(), RandomForestClassifier(), SVC(), KNeighborsClassifier()]
+
+for model in models:
+	model.fit(X_train, y_train)
+	f1 = f1_score(y_eval, model.predict(X_eval), pos_label='AF')
+	res = res.append({'model': f"{model}", 'f1': f1}, ignore_index=True)
+
+st.write(res.sort_values('f1', ascending=False))
+print(res.sort_values('f1', ascending=False))
---
+++ model_selection.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+import pandas as pd
+from urllib.parse import urlparse
+# MlFlow
+import mlflow
+import mlflow.sklearn
+# Metrics
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.metrics import f1_score, fbeta_score, make_scorer, confusion_matrix
+# Models
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+lead = sys.argv[1] if len(sys.argv) > 1 else 'lead2-HRV'
+q = float(sys.argv[2]) if len(sys.argv) > 2 else 0.99
+
+def load_data(lead):
+	df = pd.read_feather(f'datasets/phys-raw-{lead}-corrected')
+	df = df.loc[df['age'] >= 0]
+	df.loc[df.label != 'AF', 'label'] = 'Non-AF'
+	return df
+
+def filter_df(df, q=.99):
+	df = df.copy()
+	cols = df.columns
+	cols = cols.drop('label')
+	for col in cols:
+	    df = df[df[col] < df[col].quantile(q)]
+	df = df.copy()
+	return df
+
+df_raw = load_data(lead)
+
+# to_drop = ['HRV_SDSD']
+# df_raw = df_raw.drop(to_drop, axis=1)
+
+df_raw = filter_df(df_raw, q)
