05fb7cd292c45b3a1522f9043faef9f41e644c69
Tomi <tomi.ambro94@gmail.com>
2020-10-29 22:53:51 -0300

    Adding MLproject file and conda env yaml, added working script to run experiments

--- model_selection.py
+++ model_selection.py
@@ -18,8 +18,8 @@ from sklearn.linear_model import LogisticRegression
 
 
 def load_data(lead):
-	df = pd.read_feather(f'datasets/phys-raw-{lead}-corrected')
-	df = df.loc[df['age'] >= 0]
+	df = pd.read_feather(f'datasets/phys-raw-{lead}-eda')
+	df = df.drop(to_drop, axis=1)
 	df.loc[df.label != 'AF', 'label'] = 'Non-AF'
 	return df
 
@@ -29,8 +29,9 @@ def filter_df(df, q):
 	cols = cols.drop('label')
 	for col in cols:
 	    df = df[df[col] < df[col].quantile(q)]
-	df = df.copy()
 	return df
+
+
 
 if __name__ == '__main__':
 	lead = sys.argv[1] if len(sys.argv) > 1 else 'lead2-HRV'
@@ -38,3 +39,38 @@ if __name__ == '__main__':
 	
 	df_raw = load_data(lead)
 	df_raw = filter_df(df_raw, q)
+	y = df_raw['label']
+	X = df_raw.drop('label', axis=1)
+	X_train, X_eval, y_train, y_eval = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+	models = [LogisticRegression(max_iter=2000, n_jobs=4), RandomForestClassifier(n_jobs=4), SVC(), KNeighborsClassifier(n_jobs=4)]
+
+	for model in models:
+		model.fit(X_train, y_train)
+		f1 = f1_score(y_eval, model.predict(X_eval), pos_label='AF')
+		# res = res.append({'model': f"{model}", 'f1': f1}, ignore_index=True)
+
+		with mlflow.start_run():
+			print(f"Model {model}")
+			print(f"  F1: {f1}")
+
+			mlflow.log_param("model", f"{model}")
+			mlflow.log_param("lead", lead)
+			mlflow.log_param("quantile", q)
+			mlflow.log_param("columns", list(X.columns))
+			mlflow.log_param("number columns", len(list(X.columns)))
+			mlflow.log_metric("f1", f1)
+
+			tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+	        # Model registry does not work with file store
+			if tracking_url_type_store != "file":
+
+	            # Register the model
+	            # There are other ways to use the Model Registry, which depends on the use case,
+	            # please refer to the doc for more information:
+	            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+				mlflow.sklearn.log_model(model, "model", registered_model_name="AF_Classifier")
+			else:
+				mlflow.sklearn.log_model(model, "model")
